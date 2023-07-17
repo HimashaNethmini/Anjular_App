2505bd4e25011494b97b6b8319c2ecd2ef3badef
HimashaNethmini <himashanethmini05@gmail.com>
2023-07-17 11:21:09 +0530

    checking the functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,9 +10,42 @@ export class AppComponent {
 
   calValue: number = 0;
   funcT: any = 'NoFunction';
+  calNumber: string = 'noValue';
+  firstNumber: number = 0;
+  secondNumber: number = 0;
 
+
+  //declare parameters and variables for onclick function
   onClickValue ( val:string, type:any) {
-    console.log(val, type);
+    if ( type === 'number') {
+      this.onNumberClick(val);
+    }
+    else if (type === 'function'){
+      this.onFunctionClick(val);
+    }
+  }
+
+  onNumberClick(val: string){
+    if(this.calNumber != 'noValue'){
+      this.calNumber = this.calNumber + val;
+    }
+    else {
+      this.calNumber = val;
+    }
+    this.calValue = parseFloat(this.calNumber);
+
+  }
+
+  onFunctionClick (val:string) {
+    //checking if it's the first function input by the user
+    if(this.funcT == 'NoFunction') {
+      this.firstNumber = this.calValue;
+      this.calValue = 0;
+      this.calNumber = 'noValue';
+      this.funcT = val;
+
+
+    }
   }
 
 }
